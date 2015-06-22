f59a0532aa1116e386814e405d1267e5e1a77e18
Fengyuan Chen <cfy.net@gmail.com>
2015-06-22 23:48:11 +0800

    Update tooltip

--- js/main.js
+++ js/main.js
@@ -14,22 +14,16 @@ $(function () {
         $name.focus();
 
         // Tooltip: https://github.com/fengyuanchen/tooltip
-        $body.tooltip('show', 'Please enter a name', {
-          style: 'warning'
-        });
+        $body.tooltip('Please enter a name', 'warning');
       }
     },
     done: function (e, data) {
       if ($.isPlainObject(data) && data.success) {
-        $body.tooltip('show', data.result, {
-          style: 'success'
-        });
+        $body.tooltip(data.result, 'success');
       }
     },
     fail: function (e, textStatus) {
-      $body.tooltip('show', textStatus, {
-        style: 'danger'
-      });
+      $body.tooltip(textStatus, 'danger');
     }
   });
 });
